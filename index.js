78e0e9d7de84361cb84737f28a5f8121ada3c3b6
MouniReddy21 <81528901+MouniReddy21@users.noreply.github.com>
2021-09-29 20:08:12 +0530

    js, css updated

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const x='X', o='O';
 let playerturn = x;
+
+const turnx = document.getElementById('turnx')
+
 const turn_box = document.querySelector(".turn")
 console.log(turn_box)
 const win_tag = document.querySelector(".won")
@@ -37,8 +40,12 @@ function handleClick(e){
     else if(checkDraw()) {
         win_end(false)
     } else {
-        if (playerturn == x) playerturn = o;
-        else playerturn = x;
+        if (playerturn == x){ playerturn = o;
+        turnx.innerHTML = "O's Turn"
+        }
+        else{playerturn = x;
+        turnx.innerHTML = "X's Turn"
+        }
     }
     
 }
@@ -47,22 +54,29 @@ function player_turn(turn_e){
     const btn = document.getElementById(turn_e)
     console.log(btn)
     playerturn = x;
-    console.log("hello" + " " +typeof(playerturn))
-    highTurn(playerturn)
-    // btn.style.color = "yellow"
-    // btn.classList.add('playerturn')
-}
-
-
-function highTurn(pturn){
-    // if(pturn=='x'){
-    //     document.getElementById("turnx").style.background-color="yellow"
-    // }
-    // else{
-    //     document.getElementById("turnx").style.background-color = "yellow"
-    // }
+    // console.log("hello" + " " +typeof(playerturn))
+    showOrHideDiv()   
+    toggler('board');
 
 }
+
+function toggler(divId) {
+    $("#" + divId).toggle();   // added jquery.min.lib
+}
+function showOrHideDiv() {
+    var v = document.getElementById("turn");
+    // console.log(v.style.display)
+    // if (v.style.display === "none") {
+    //    v.style.display = "block";
+    // } else {
+    //    v.style.display = "none";
+    // }
+    v.classList.add("hide");
+    
+ }
+
+
+
 function checkDraw(){
     return [...cellElements].every(cell =>{
         return((cell.innerText).length>0)
@@ -79,6 +93,7 @@ function checkWin(pturn){
 
 function win_end(status)
 {
+
     console.log(status)
     if(status){
         win_tag.innerHTML = `Player ${playerturn=='X'?'X':'O'} Won!`
@@ -86,4 +101,6 @@ function win_end(status)
     else{
         win_tag.innerHTML = "Draw!"
     }
+    // $('[data-cell]').off('click');
+     toggler('result');
 }
